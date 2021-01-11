f338245b22a44379bc1e437daa4632fe29063a6d
sajjad Hossain <sajjadhossain0057@gamil.com>
2021-01-12 04:47:49 +0600

    Uses of ListCreateAPIView,RetrieveUpdateDestroyAPIView

--- API/status/views.py
+++ API/status/views.py
@@ -3,13 +3,9 @@ from .serializers import StatusSerializer  # Serializer Based on Status Model
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.mixins import CreateModelMixin,UpdateModelMixin,DestroyModelMixin
 from rest_framework.generics import (
-    ListAPIView,
-    CreateAPIView ,
-    RetrieveAPIView,
-    UpdateAPIView,
-    DestroyAPIView,
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
     
     )
 
@@ -21,26 +17,18 @@ from rest_framework.generics import (
 
 # By using  StatusListCreateView class we can retrive and create data 
 
-class StatusListCreateView(CreateModelMixin,ListAPIView):
+class StatusListCreateView(ListCreateAPIView):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+
     
     
     
 # By using StatusDetailAPIView class we can  retrive , update and Detele data   
     
-class StatusDetailAPIView(UpdateModelMixin,DestroyModelMixin,RetrieveAPIView):
+class StatusDetailAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
     lookup_field = "id"
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
     
     
